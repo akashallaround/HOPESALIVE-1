9014f295edec38a3ae0f4e0daa698ddf3b3ed8f3
ayushh124 <ayushh124@gmail.com>
2025-03-21 22:29:44 +0530

    Update frontend folder with new files

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://hopesalive-lldc.vercel.app', // replace with your backend URL
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
